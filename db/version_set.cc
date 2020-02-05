154054ff42b53d36a9cc60423bcecca10bcdaa55
Zhichao Cao <zhichao@fb.com>
2020-02-05 14:43:55 -0800

    Change Current file write return with IOStatus and related others

--- db/version_set.cc
+++ db/version_set.cc
@@ -3747,6 +3747,7 @@ Status VersionSet::ProcessManifestWrites(
 
   uint64_t new_manifest_file_size = 0;
   Status s;
+  IOStatus io_s;
 
   assert(pending_manifest_file_number_ == 0);
   if (!descriptor_log_ ||
@@ -3850,7 +3851,6 @@ Status VersionSet::ProcessManifestWrites(
         }
         ++idx;
 #endif /* !NDEBUG */
-        IOStatus io_s;
         io_s = descriptor_log_->AddRecord(record);
         if (!io_s.ok()) {
           io_status_ = io_s;
@@ -3858,7 +3858,6 @@ Status VersionSet::ProcessManifestWrites(
           break;
         }
       }
-      IOStatus io_s;
       if (s.ok()) {
         io_s = SyncManifest(env_, db_options_, descriptor_log_->file());
       }
@@ -3873,8 +3872,12 @@ Status VersionSet::ProcessManifestWrites(
     // If we just created a new descriptor file, install it by writing a
     // new CURRENT file that points to it.
     if (s.ok() && new_descriptor_log) {
-      s = SetCurrentFile(env_, dbname_, pending_manifest_file_number_,
+      io_s = SetCurrentFile(fs_, dbname_, pending_manifest_file_number_,
                          db_directory);
+      if (!io_s.ok()) {
+        io_status_ = io_s;
+        s = io_s;
+      }
       TEST_SYNC_POINT("VersionSet::ProcessManifestWrites:AfterNewManifest");
     }
 
